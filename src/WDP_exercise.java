a5f44b550b7386ff541fe9a4e81777bd592a186b
romanekP <romanekpawel@gmail.com>
2022-09-20 18:21:04 +0200

    added int wiht age specification

--- src/WDP_exercise.java
+++ src/WDP_exercise.java
@@ -3,15 +3,21 @@ import java.util.Scanner;
 public class WDP_exercise {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter your name: ");
+        System.out.println("Enter your Name: ");
         String name = scanner.next();
-        System.out.println("Enter your lastname: ");
+        System.out.println("Enter your Lastname: ");
         String lastname = scanner.next();
-        System.out.println("Enter your age: ");
-        int age = scanner.nextInt();
-        if (age < 21) {
-            System.out.println(name + " " + lastname + " You're too young to enter :-(");
-        } else {
+        System.out.println("Enter your Age: ");
+        int Age = scanner.nextInt();
+        if (Age < 21) {
+            System.out.println(name + " " + lastname +
+                    "You're too young to enter :-(");
+        }
+        if (Age==21){
+            System.out.println(name + " " + lastname +
+                    " Congratulations! Barely, barely... ");
+        }
+        else {
             System.out.println(name + " " + lastname + " You can enter!");
         }
 
